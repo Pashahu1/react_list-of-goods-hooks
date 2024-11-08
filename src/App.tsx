3c59dec366c09a4ebcbbf024ca1c7fb49f4a060c
Pavlo Chudin <maks.chudin567@gmail.com>
2024-11-08 02:11:15 +0200

    create a static(change)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { GoodList } from './component/GoodList';
+import cn from 'classnames';
+import { IListOfGoodsHooks } from './types/IListOfGoodsHooks';
 
-export const goodsFromServer = [
+enum SortType {
+  Alphabetically = 'alphabetically',
+  Length = 'length',
+}
+
+export const goodsFromServer: string[] = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -15,37 +23,82 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App: React.FC = () => {
+export const App: React.FC<IListOfGoodsHooks> = () => {
+  const [filteredGoods] = useState<string[]>(goodsFromServer);
+  const [correctSortField, setCorrectSortField] = useState<string>('');
+  const [correctReverse, setCorrectReverse] = useState<boolean>(false);
+
+  const classAl = { 'is-light': correctSortField !== SortType.Alphabetically };
+  const classLength = { 'is-light': correctSortField !== SortType.Length };
+
+  function getPreparedGoods(goods: string[]) {
+    const sortedGoods = [...goods];
+
+    switch (correctSortField) {
+      case SortType.Alphabetically:
+        sortedGoods.sort((good1, good2) => good1.localeCompare(good2));
+        break;
+      case SortType.Length:
+        sortedGoods.sort((good1, good2) => good1.length - good2.length);
+        break;
+      default:
+        break;
+    }
+
+    if (correctReverse) {
+      sortedGoods.reverse();
+    }
+
+    return sortedGoods;
+  }
+
+  const visibleGoods = getPreparedGoods(filteredGoods);
+
   return (
     <div className="section content">
       <div className="buttons">
-        <button type="button" className="button is-info is-light">
+        <button
+          type="button"
+          className={cn('button is-success', classAl)}
+          onClick={() => {
+            setCorrectSortField(SortType.Alphabetically);
+          }}
+        >
           Sort alphabetically
         </button>
 
-        <button type="button" className="button is-success is-light">
+        <button
+          type="button"
+          className={cn('button is-info', classLength)}
+          onClick={() => {
+            setCorrectSortField(SortType.Length);
+          }}
+        >
           Sort by length
         </button>
 
-        <button type="button" className="button is-warning is-light">
+        <button
+          type="button"
+          className={cn('button is-warning', { 'is-light': !correctReverse })}
+          onClick={() => setCorrectReverse(!correctReverse)}
+        >
           Reverse
         </button>
 
-        <button type="button" className="button is-danger is-light">
-          Reset
-        </button>
+        {(correctSortField || correctReverse) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => {
+              setCorrectSortField('');
+              setCorrectReverse(false);
+            }}
+          >
+            Reset
+          </button>
+        )}
       </div>
-
-      <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
-      </ul>
+      <GoodList goods={visibleGoods} />
     </div>
   );
 };
